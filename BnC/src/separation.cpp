a1e85fe69afab5b6ce10100c76a6b229552d1ad4
Franky03 <kaikym20@gmail.com>
2024-12-10 11:38:53 -0300

    max back done

--- BnC/src/separation.cpp
+++ BnC/src/separation.cpp
@@ -17,20 +17,12 @@ vector<int> findInitialSubset(int n, set<int>& excluded){
 }
 
 vector <vector<int> > MaxBack(double** x, int n){
-    std::cout << "MaxBack" << std::endl;
     vector<vector<int>> result; // Armazena todos os subconjuntos best_S encontrados
     set<int> excluded;          // Conjunto de vértices já escolhidos inicialmente
 
-    srand(time(nullptr)); // Inicializa o gerador de números aleatórios
-    std::cout << "DEBUG 1"<< std::endl;
-
     while (excluded.size() < n) {
-
-        std::cout << "DEBUG 2"<< std::endl;
         vector<int> S0 = findInitialSubset(n, excluded);
         set<int> S(S0.begin(), S0.end());
-
-        std::cout << "DEBUG 3"<< std::endl;
 
         set<int> complement;
         for(int i = 0; i < n; ++i){
@@ -39,13 +31,14 @@ vector <vector<int> > MaxBack(double** x, int n){
             }
         }
 
-        std::cout << "DEBUG 4"<< std::endl;
-
         vector<double> maxback_val(n, 0.0);
 
         for(int i: complement){
             for(int j: S){
-                maxback_val[i] += x[i][j];
+                if(j > i)
+                    maxback_val[i] += x[i][j];
+                else
+                    maxback_val[i] += x[j][i];
             }
         }
 
@@ -53,25 +46,22 @@ vector <vector<int> > MaxBack(double** x, int n){
         double mincut_val;
         set<int> best_S = S;
 
-        std::cout << "DEBUG 5"<< std::endl;
-
         // calcula valores iniciais 
         for(int i: S){
             for(int j: complement){
-                cut_val += x[i][j];
+                if(j > i)
+                    cut_val += x[i][j];
+                else
+                    cut_val += x[j][i];
             }
         }
         mincut_val = cut_val;
 
-        std::cout << "DEBUG 6"<< std::endl;
-
         // main loop
-        for(int k = 0; k < n - S0.size(); ++k){
-            std::cout << "DEBUG 7"<< std::endl;
+        for(int k = 0; k < n - 1; ++k){
             // encontra o vértice com maior maxback_val
             int selected_vertex = -1;
             double max_val = -1.0;  
-            std::cout << excluded.size() << std::endl;
             for(int i: complement){
                 if(maxback_val[i] > max_val){
                     max_val = maxback_val[i];
@@ -82,8 +72,6 @@ vector <vector<int> > MaxBack(double** x, int n){
             if(selected_vertex == -1){
                 break;
             }
-            
-            std::cout << "Selected vertex: " << selected_vertex << std::endl;
 
             // atualiza S e complement
             S.insert(selected_vertex);
@@ -94,7 +82,11 @@ vector <vector<int> > MaxBack(double** x, int n){
 
             // atualiza maxback_val
             for (int j : complement) { // complement representa V \ S_k
-                maxback_val[j] += x[selected_vertex][j]; // selected_vertex é o vértice recém-adicionado (i)
+                if (j > selected_vertex) {
+                    maxback_val[j] += x[selected_vertex][j];
+                } else {
+                    maxback_val[j] += x[j][selected_vertex];
+                }
             }
 
             // atualiza mincut_val
@@ -103,14 +95,14 @@ vector <vector<int> > MaxBack(double** x, int n){
                 best_S = S;
             } 
         }
-        std::cout << "DEBUG 8"<< std::endl;
+
         if (!best_S.empty() && best_S.size() < n) {
             // Adiciona o melhor subconjunto encontrado ao resultado apenas se ele não for vazio ou completo
             result.push_back(vector<int>(best_S.begin(), best_S.end()));
         }
 
         excluded.insert(best_S.begin(), best_S.end());
-        std::cout << "DEBUG 9"<< std::endl;
+
     }
     
     return result;
